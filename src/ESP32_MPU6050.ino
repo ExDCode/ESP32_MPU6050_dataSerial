eb0b450041257ac48d76ad2e2ddf5dbe1d00fbbd
ExDCode <exdcode@gmail.com>
2022-08-08 17:46:50 -0700

    small text and filename clean up

--- src/ESP32_MPU6050.ino
+++ src/ESP32_MPU6050.ino
@@ -1,9 +1,3 @@
-/*
- *  This sketch demonstrates how to scan WiFi networks.
- *  The API is almost the same as with the WiFi Shield library,
- *  the most obvious difference being the different file you need to include:
- */
-#include "WiFi.h"
 #include <Adafruit_Sensor.h>
 #include <Adafruit_MPU6050.h>
 #include <Wire.h>
@@ -14,23 +8,7 @@ void setup()
 {
     Serial.begin(115200);
 
-    // // Set WiFi to station mode and disconnect from an AP if it was previously connected
-    // WiFi.mode(WIFI_STA);
-    // WiFi.disconnect();
-    // delay(100);
-
-    // Serial.println("Setup done");
-
-// 
-    // Serial.begin(115200);
-
-    // -------------------------
-    // WiFi.mode(WIFI_STA);
-    // WiFi.disconnect();
-    // delay(100);
-    // -------------------------
-
-    while (!Serial)
+        while (!Serial)
         delay(10); // will pause Zero, Leonardo, etc until serial console opens
 
     Serial.println("Adafruit MPU6050 test!");
@@ -109,33 +87,6 @@ void setup()
 
 void loop()
 {
-    // Serial.println("scan start");
-
-    // // WiFi.scanNetworks will return the number of networks found
-    // int n = WiFi.scanNetworks();
-    // Serial.println("scan done");
-    // if (n == 0) {
-    //     Serial.println("no networks found");
-    // } else {
-    //     Serial.print(n);
-    //     Serial.println(" networks found");
-    //     for (int i = 0; i < n; ++i) {
-    //         // Print SSID and RSSI for each network found
-    //         Serial.print(i + 1);
-    //         Serial.print(": ");
-    //         Serial.print(WiFi.SSID(i));
-    //         Serial.print(" (");
-    //         Serial.print(WiFi.RSSI(i));
-    //         Serial.print(")");
-    //         Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-    //         delay(10);
-    //     }
-    // }
-    // Serial.println("");
-
-    // // Wait a bit before scanning again
-    // delay(5000);
-
     /* Get new sensor events with the readings */
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
